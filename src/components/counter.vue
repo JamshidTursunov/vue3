8ece7b62dc479296063dcbd77a9b6b0c90e88311
Jamshid Tursunov <u1810153@mail.ru>
2021-03-01 23:35:51 +0500

    first commit

---
+++ src/components/counter.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="counter">
+    <button>+</button>
+    {{ counter }}
+    <button>-</button>
+    <h1>Counter value: {{ counter }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      counter: 0,
+      x: null,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.counter {
+  padding: 1rem 1rem;
+  border: 1px solid red;
+}
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,25 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="container about">
+    <div class="row">
+      <div class="col-md-12">
+        <h1>This is about page</h1>
+      </div>
+    </div>
+    <counter class="col-md-4" />
   </div>
 </template>
+
+<script>
+import counter from "../components/counter";
+
+export default {
+  components: {
+    counter
+  },
+  data() {
+    return {
+      x: null
+    };
+  }
+};
+</script>
